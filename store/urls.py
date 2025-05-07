3664b2f61a5b03e9161751b905472eae5188059d
iamtoto <jackwj368@gmail.com>
2025-05-07 16:35:27 -0500

    set up most homepage html

--- store/urls.py
+++ store/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('favorites/', views.favorites, name='favorites'),
+    path('mood/<str:mood>/', views.shop_by_mood, name='shop_by_mood'),
+    path('genre/<str:genre>/', views.shop_by_genre, name='shop_by_genre'),
 ]
--- store/views.py
+++ store/views.py
@@ -4,3 +4,23 @@ from .models import Product
 def home(request):
     products = Product.objects.all()
     return render(request, 'store/home.html', {'products': products})
+
+def favorites(request):
+    # TODO: replace with real query & ordering
+    return render(request, 'store/favorites.html', {
+        # 'beats': beats
+    })
+
+def shop_by_mood(request, mood):
+    # TODO: filter your Product model by mood
+    return render(request, 'store/shop_by_mood.html', {
+        # 'beats': beats,
+        # 'selected_mood': mood
+    })
+
+def shop_by_genre(request, genre):
+    # TODO: filter your Product model by genre
+    return render(request, 'store/shop_by_genre.html', {
+        # 'beats': beats,
+        # 'selected_genre': genre
+    })
