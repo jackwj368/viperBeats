1a564d68813efc5ba14fd7aac7bec4599e4b290f
iamtoto <jackwj368@gmail.com>
2025-05-07 11:59:26 -0500

    made product model n a little extra

--- store/models.py
+++ store/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    audio_file = models.FileField(upload_to='wavs/')
+
+    def __str__(self):
+        return self.title
